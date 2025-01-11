a3fe713502c45d7b2fc74e13d5f489e77cc91961
Axel-C19 <axelmoises4@gmail.com>
2025-01-10 17:56:51 -0800

    Implementacion de la template y cambios al boton read state

--- frontend/README.md
+++ frontend/README.md
@@ -1,8 +1,25 @@
-# React + Vite
+## Vite-Template
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+To install the dependencies you put "yarn" in the console, and to run it you use "yarn start"
 
-Currently, two official plugins are available:
+In order to use the example, the following must be followed:
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+1. First you need to have the name and mnemonic of the wallet (account) which will pay for the creation of vouchers, etc.
+2. In the file within "src/app/consts.ts", you will have to put the data obtained from the account that will cover the payments.
+3. The information will have to be put on lines 23 and 24:
+
+```javascript
+export const sponsorName = "";
+export const sponsorMnemonic = "";
+```
+
+> Note: The template contains a sponsor example that contains tokens, for your dApp, you need to put a sponsor name and a sponsor mnemonic
+
+4. With this, you can now use the example in the template.
+5. You can try the frontend template on gitpod!
+
+<p align="center">
+  <a href="https://gitpod.io/#https://github.com/Vara-Lab/Chackra-UI-Vite-Sails-Template.git" target="_blank">
+    <img src="https://gitpod.io/button/open-in-gitpod.svg" width="240" alt="Gitpod">
+  </a>
+</p>
---
+++ frontend/src/app/consts.ts
@@ -0,0 +1,77 @@
+import { HexString } from '@gear-js/api';
+
+interface ContractSails {
+  programId: HexString,
+  idl: string
+}
+
+export const ACCOUNT_ID_LOCAL_STORAGE_KEY = 'account';
+
+export const ADDRESS = {
+  NODE: import.meta.env.VITE_NODE_ADDRESS,
+  BACK: import.meta.env.VITE_BACKEND_ADDRESS,
+  GAME: import.meta.env.VITE_CONTRACT_ADDRESS as HexString,
+};
+
+export const ROUTES = {
+  HOME: '/',
+  EXAMPLES: '/examples',
+  NOTFOUND: '*',
+};
+
+// To use the example code, enter the details of the account that will pay the vouchers, etc. (name and mnemonic)
+// Here, you have an example account that contains tokens, in your dApp, you need to put a sponsor name
+// and a sponsor mnemonic
+export const sponsorName = 'Alice';
+export const sponsorMnemonic = 'bottom drive obey lake curtain smoke basket hold race lonely fit walk';
+
+export const CONTRACT_DATA: ContractSails = {
+  programId: '0xb3809b8c18422c3a8fc6e3452b11234dea7cddbfd073c427f42e1ed08408cdb7',
+  idl: `
+    type ExpenseDto = struct {
+  description: str,
+  amount: u128,
+  currency: str,
+};
+
+type Events = enum {
+  GroupCreated: u32,
+  UserJoined: struct { actor_id, u32 },
+  ExpenseAdded: struct { u32, u32 },
+  Error: str,
+};
+
+type IoState = struct {
+  admins: vec actor_id,
+  groups: vec Group,
+};
+
+type Group = struct {
+  id: u32,
+  members: vec actor_id,
+  expenses: vec Expense,
+};
+
+type Expense = struct {
+  id: u32,
+  description: str,
+  amount: u128,
+  currency: str,
+  actor_id: actor_id,
+};
+
+constructor {
+  New : ();
+};
+
+service Service {
+  AddExpense : (group_id: u32, expenseDTO: ExpenseDto) -> Events;
+  CreateGroup : () -> Events;
+  JoinGroup : (group_id: u32) -> Events;
+  query Query : () -> IoState;
+  query QueryAdmins : () -> vec actor_id;
+  query QueryExpenses : (group_id: u32) -> opt vec Expense;
+  query QueryGroupMembers : (group_id: u32) -> opt vec actor_id;
+};
+  `
+};
